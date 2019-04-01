af0ea7804574d352b36075f806e3031de7b47def
delly <delly@dellyhost.dellydomain>
2019-04-01 13:06:47 -0500

    header styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; 
+import './app.css';
 
-import Header from './components/header';
+import Header from '../header';
 
 class App extends Component {
   render() {
