f8568fd048267425da108a53874c3a363deba354
trif3 <trifonzafiridis@gmail.com>
2019-04-06 15:17:09 +0300

    rollin' random athlete

--- src/components/random-athlete/random-athlete.js
+++ src/components/random-athlete/random-athlete.js
@@ -1,48 +1,68 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import DojoService from '../../services/dojo-service';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 import './random-athlete.css'
-export default class RandomAthlete extends Component {
 
+export default class RandomAthlete extends Component {
+    
     static defaultProps = {
-        updateInterval: 1000
+        updateInterval: 10000
     };
 
     static propTypes = {
-
+        updateInterval: PropTypes.number
     };
 
     dojoService = new DojoService()
- 
-    state = { 
-        athlete: {}, 
-        loading: true
-     };
 
-    constructor() {
-        super();
+    state = {
+        athlete: {},
+        loading: true
+    };
+
+    componentDidMount() {
+        const {updateInterval} = this.props;
         this.updateAthlete();
+        this.interval = setInterval(this.updateAthlete, updateInterval);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     onAthleteLoaded = (athlete) => {
         this.setState({
             athlete, 
-            loading: false
+            loading: false,
+            error: false
         });
     }
 
-    updateAthlete(){
+    onError = (err) => { 
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updateAthlete = () => {
         this.dojoService
             .getAthlete()
-            .then(this.onAthleteLoaded);
+            .then(this.onAthleteLoaded)
+            .catch(this.onError);
     }
 
     render() {
-        const { athlete, loading } = this.state;
+        const { athlete, loading, error } = this.state;
+        const hasAthlete = !(loading || error);
+        const errorMessage = error ? <ErrorIndicator/> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <AthleteView athlete={athlete}/> : null;
+        const content = hasAthlete ? <AthleteView athlete={athlete}/> : null;
         return (
             <div className="random-athlete jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
