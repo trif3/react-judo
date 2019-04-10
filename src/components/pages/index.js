c01cfe43c9503d553fea66095bdfa33cd0457062
trif3 <trifonzafiridis@gmail.com>
2019-04-10 16:25:35 +0300

    pages indexing file

---
+++ src/components/pages/index.js
@@ -0,0 +1,9 @@
+import AthletePage from './athlete-page';
+import TeacherPage from './teacher-page';
+import ChampionshipPage from './championship-page';
+
+export {
+    AthletePage,
+    TeacherPage,
+    ChampionshipPage
+};
