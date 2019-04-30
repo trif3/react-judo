090aca6b42532643fc4327d065c59684c5a751a0
trif3 <trifonzafiridis@gmail.com>
2019-04-30 20:37:34 +0300

    consistency

--- src/components/nav-bar/navBar.js
+++ src/components/nav-bar/navBar.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-
-// import logo from '../../imgs/logo.svg';
-
 import './navbar.css';
 
 export default class NavBar extends Component {
