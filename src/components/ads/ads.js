c3362f9fe5667e66bb446c01c5a54cf03e7f1d89
trif3 <trifonzafiridis@gmail.com>
2019-05-02 13:12:48 +0300

    ads code unit

--- src/components/ads/ads.js
+++ src/components/ads/ads.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 export default class Ads extends Component {
     componentDidMount() {
-        (window.adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-8195032248780484",
-            enable_page_level_ads: true
-       });
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
     }
 
     render() {
         return (
-            <div>
-                ads
-            </div>
+            <ins className="adsbygoogle"
+                style={{display:'block'}}
+                data-ad-client="ca-pub-8195032248780484"
+                data-ad-slot="6837914201"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>   
         );
     }
 }
