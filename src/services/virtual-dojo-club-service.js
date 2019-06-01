7c983fe677eb9eed7e79aa3f0493bbfef576d7d3
trif3 <trifonzafiridis@gmail.com>
2019-06-01 17:05:30 +0300

    dojo info

--- src/services/virtual-dojo-club-service.js
+++ src/services/virtual-dojo-club-service.js
@@ -4,8 +4,8 @@ export default class VirtualDojoClubService {
             dojos: [
                 {
                     id: 1,
-                    name: "J one",
-                    details: "more info",
+                    name: "Dojo One",
+                    details: "Dojo one is the main dojo",
                     athletes: [
                         {
                             id: 1,
@@ -16,8 +16,9 @@ export default class VirtualDojoClubService {
                                 {
                                     medal: "gold",
                                     date: "2019-01-01",
-                                    city: "thessaloniki",
-                                    info: "championship info"
+                                    city: "Thessaloniki",
+                                    info: "Thessaloniki Championship",
+                                    kg: 50
                                 }
                             ]
                         },
@@ -31,7 +32,8 @@ export default class VirtualDojoClubService {
                                     medal: "gold",
                                     date: "2019-01-01",
                                     city: "LA",
-                                    info: "championship info"
+                                    info: "Thessaloniki Championship",
+                                    kg: 62
                                 }
                             ]
                         }
@@ -47,7 +49,8 @@ export default class VirtualDojoClubService {
                                     medal: "gold",
                                     date: "2019-01-01",
                                     city: "Ath",
-                                    info: "championship info"
+                                    info: "Japan Championship",
+                                    kg: 50
                                 }
                             ]
                         },                    
@@ -61,13 +64,98 @@ export default class VirtualDojoClubService {
                                     medal: "gold",
                                     date: "2019-01-01",
                                     city: "RU",
-                                    info: "championship info"
+                                    info: "Russian Championship",
+                                    kg: 50
                                 }
                             ]
                         }
                     ],
                     championships : [
-
+                        {
+                            id: 5, 
+                            name: "Thessaloniki Championship Mega Ippon",
+                            city: "Thessaloniki",
+                            country: "Greece",
+                            date: "2015-05-06"
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    name: "Dojo Second",
+                    details: "Dojo Second is the second dojo",
+                    athletes: [
+                        {
+                            id: 1,
+                            name: "Lenny",
+                            gender: "ms",
+                            age: 24,
+                            distinction: [
+                                {
+                                    medal: "gold",
+                                    date: "2019-01-01",
+                                    city: "Thessaloniki",
+                                    info: "Thessaloniki Championship",
+                                    kg: 50
+                                }
+                            ]
+                        },
+                        {
+                            id: 2,
+                            name: "Kenny",
+                            gender: "ms",
+                            age: 26,
+                            distinction: [
+                                {
+                                    medal: "gold",
+                                    date: "2019-01-01",
+                                    city: "LA",
+                                    info: "Thessaloniki Championship",
+                                    kg: 62
+                                }
+                            ]
+                        }
+                    ],
+                    teachers: [
+                        {
+                            id: 3,
+                            name: "Tenny",
+                            gender: "ms",
+                            age: 38,
+                            distinction: [
+                                {
+                                    medal: "gold",
+                                    date: "2019-01-01",
+                                    city: "Ath",
+                                    info: "Japan Championship",
+                                    kg: 50
+                                }
+                            ]
+                        },                    
+                        {
+                            id: 4,
+                            name: "Menny",
+                            gender: "ms",
+                            age: 40,
+                            distinction: [
+                                {
+                                    medal: "gold",
+                                    date: "2019-01-01",
+                                    city: "RU",
+                                    info: "Russian Championship",
+                                    kg: 50
+                                }
+                            ]
+                        }
+                    ],
+                    championships : [
+                        {
+                            id: 5, 
+                            name: "Thessaloniki Championship Mega Ippon",
+                            city: "Thessaloniki",
+                            country: "Greece",
+                            date: "2015-05-06"
+                        }
                     ]
                 },
             ]
