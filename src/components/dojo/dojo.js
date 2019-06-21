8bacc8e894d40a8a2401ef8b15803998832b93db
trif3 <trifonzafiridis@gmail.com>
2019-06-21 19:27:12 +0300

    smooth

--- src/components/dojo/dojo.js
+++ src/components/dojo/dojo.js
@@ -9,15 +9,14 @@ const Dojo = ( {dojo} ) => {
     if(id%2 === 0){
         dojoIco = <FaMedal />
     }
-    console.log(brandImage)
     return (
         <div className="club-dojo">
             <div className="brand-image">
                 <img src={brandImage} alt="brand" />
             </div>
             <div className="club-dojo-details">
-                <a href="#">{id}. {dojoIco} </a>
-                <a href="#" className="dojo-name">{name}</a>
+                <span >{id}. {dojoIco} </span>
+                <span className="dojo-name">{name}</span>
                 <div className="dojo-details">{details}</div>
                 <div className="dojo-address">{address}</div>
                 <button className="btn btn-info go-to-dojo">Go to Dojo</button>
