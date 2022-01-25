1c63e4514cba840cb0cfc7415aed75660c89b2e5
Mrbeamoo <mrbeamo@hotmail.com>
2022-01-26 00:04:20 +0200

    Init Contact Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Main from "./components/Main/Main";
+import Contact from "./components/Contact/Contact";
 
 const App = () => (
   <div className="App">
     <Main />
+    <Contact /> 
   </div>
 );
 
 export default App;
+
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+const Contact = () => (
+    <h1>Contact us today</h1>
+);
+
+export default Contact;
