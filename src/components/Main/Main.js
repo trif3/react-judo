8eeb971717052dc95d69667a7e1fc61def93097b
OlgaKaragkiozidou <karagkiozidouolga@gmail.com>
2022-02-09 19:58:03 +0200

    parallax bg edit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,20 +4,22 @@ import Nav from "../Navbar/Nav";
 import Image from "./ImageBg";
 import Overlay from "./Overlay";
 import Info from "./Info";
+import ImageBg from "../Image/blackbeltBg.jpg";
 
 const Main = () => (
   <section css={styles} className="main" id="home">
     <Overlay />
     <Nav />
     <Info />
-    <Image />
+    
   </section>
 );
 
 const styles = css`
   width: 100%;
   height: 100vh;
-  
+  background: url('${ImageBg}') no-repeat center/cover;
+  background-attachment: fixed;
 `;
 
 export default Main;
